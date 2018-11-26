a8a105304549c8690e24b30d88e331981fd3e7ce
newcomet-lab <newcometcto729@gmail.com>
2018-11-25 21:15:52 -0500

    Create SVG where chart will be displayed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 function displayChart() {
 
   axios.get('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json').then((dataset) => {
+    const w = 700;
+    const h = 500;
+    const svg = d3.select('.chart')
+      .append('svg')
+      .attr('width', w)
+      .attr('height', h);
   }).catch(() => {
   });
 }
