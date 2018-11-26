a8da80541885c86a6e3f13818b7b702981303ae4
newcomet-lab <newcometcto729@gmail.com>
2018-11-25 23:42:04 -0500

    Add a mouseover and mouseout over each bar that changes its fill color and shows a tooltip

--- js/script.js
+++ js/script.js
@@ -46,8 +46,40 @@ function displayChart() {
       .attr('y', (d) => yScale(d[1]))
       .attr('width', w/275)
       .attr('height', (d) => h - yScale(d[1]) - padding.top)
-      .attr('fill', '#522d86');
+      .attr('fill', '#522d86')
+      .on('mouseover', handleMouseover)
+      .on('mouseout', handleMouseout);
 
+    function handleMouseover(d) {
+      const quarter = {
+        '01': 1,
+        '04': 2,
+        '07': 3,
+        '10': 4
+      };
+      const date = moment(d[0]);
+      const tooltip = d3.select('.chart')
+        .append('div')
+        .attr('class', 'tooltip')
+        .style('opacity', 0)
+
+      d3.select(this)
+        .attr('fill', '#46d246');
+
+      tooltip.transition()
+        .duration(200)
+        .style('opacity', 0.9);
+      tooltip.html(`${date.format('YYYY')} Q${quarter[date.format('MM')]}<br/>$${d[1]}B`)
+        .style('left', `${d3.event.pageX}px`)
+        .style('top', `${d3.event.pageY - 28}px`);
+    }
+
+    function handleMouseout() {
+      d3.select(this)
+        .attr('fill', '#522d86');
+
+      d3.select('.tooltip').remove();
+    }
   }).catch(() => {
   });
 }
