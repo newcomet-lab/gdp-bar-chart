6dcde5f835fa8331470c5347bea5d429687626c6
newcomet-lab <newcometcto729@gmail.com>
2018-12-15 19:32:58 -0500

    Make D3 chart responsive

--- js/script.js
+++ js/script.js
@@ -2,38 +2,33 @@ function displayChart() {
 
   axios.get('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json').then((dataset) => {
     const data = dataset.data.data;
-    const w = 700;
-    const h = 500;
     const padding = {
       top: 40,
       right: 30,
       bottom: 30,
       left: 100
     };
+    let w;
+    let h;
+
     const xScale = d3.scaleTime()
-      .domain(d3.extent(data, (d) => new Date(d[0])))
-      .range([padding.left, w - padding.right]);
+      .domain(d3.extent(data, (d) => new Date(d[0])));
     const yScale = d3.scaleLinear()
-      .domain([0, d3.max(data, (d) => d[1])])
-      .range([h - padding.top, padding.bottom]);
+      .domain([0, d3.max(data, (d) => d[1])]);
     const svg = d3.select('.chart')
-      .append('svg')
-      .attr('width', w)
-      .attr('height', h);
+      .append('svg');
 
     svg.append('g')
-      .attr('class', 'x-axis')
-      .attr('transform', `translate(0, ${h - padding.top})`)
-      .call(d3.axisBottom(xScale));
+      .attr('class', 'x-axis');
 
     svg.append('g')
       .attr('class', 'y-axis')
-      .attr('transform', `translate(${padding.left}, 0)`)
-      .call(d3.axisLeft(yScale));
+      .attr('transform', `translate(${padding.left}, 0)`);
 
     svg.append('text')
+      .attr('class', 'y-label')
       .attr('transform', 'rotate(-90)')
-      .attr('x', -400)
+      .attr('x', -280)
       .attr('y', 30)
       .text('Gross Domestic Product in Billions');
 
@@ -42,10 +37,6 @@ function displayChart() {
       .enter()
       .append('rect')
       .attr('class', 'bar')
-      .attr('x', (d) => xScale(new Date(d[0])))
-      .attr('y', (d) => yScale(d[1]))
-      .attr('width', w/275)
-      .attr('height', (d) => h - yScale(d[1]) - padding.top)
       .attr('fill', '#522d86')
       .on('mouseover', handleMouseover)
       .on('mouseout', handleMouseout);
@@ -64,8 +55,8 @@ function displayChart() {
         .duration(200)
         .style('opacity', 0.9);
       tooltip.html(`${d[0].split('-')[0]} Q${quarter.indexOf(d[0].split('-')[1]) + 1}<br/>$${d[1]}B`)
-        .style('left', `${d3.event.pageX + 8}px`)
-        .style('top', `${d3.event.pageY - 28}px`);
+        .style('left', `${d3.event.pageX - 130}px`)
+        .style('top', `${d3.event.pageY - 200}px`);
     }
 
     function handleMouseout() {
@@ -74,6 +65,39 @@ function displayChart() {
 
       d3.select('.tooltip').remove();
     }
+
+    function resize() {
+      w = parseInt(d3.select('.chart').style('width')) * 0.9;
+      h = parseInt(d3.select('.chart').style('height'));
+
+      if (w < 600) {
+        w = 600;
+      }
+
+      xScale.range([padding.left, w - padding.right]);
+      yScale.range([h - padding.top, padding.bottom]);
+
+      svg.attr('width', w)
+        .attr('height', h);
+
+      svg.select('.x-axis')
+        .attr('transform', `translate(0, ${h - padding.top})`)
+        .call(d3.axisBottom(xScale));
+
+      svg.select('.y-axis')
+        .call(d3.axisLeft(yScale));
+
+      svg.selectAll('.bar')
+        .attr('x', (d) => xScale(new Date(d[0])))
+        .attr('y', (d) => yScale(d[1]))
+        .attr('width', w / data.length)
+        .attr('height', (d) => h - yScale(d[1]) - padding.top);
+    }
+
+    resize();
+
+    d3.select(window)
+      .on('resize', resize);
   }).catch(() => {
     document.querySelector('.error-message').style.display = 'block';
   });
