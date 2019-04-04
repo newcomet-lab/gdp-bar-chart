d41c4f81a14591595395e74bb0ac219157772aae
newcomet-lab <newcometcto729@gmail.com>
2019-04-04 18:48:55 -0400

    Update UI

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ function displayChart() {
   axios.get('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json').then((dataset) => {
     const data = dataset.data.data;
     const padding = {
-      top: 40,
-      right: 30,
-      bottom: 30,
-      left: 100
+      top: 50,
+      right: 40,
+      bottom: 40,
+      left: 110
     };
     let w;
     let h;
@@ -28,8 +28,8 @@ function displayChart() {
     svg.append('text')
       .attr('class', 'y-label')
       .attr('transform', 'rotate(-90)')
-      .attr('x', -280)
-      .attr('y', 30)
+      .attr('x', -290)
+      .attr('y', 40)
       .text('Gross Domestic Product in Billions');
 
     svg.selectAll('rect')
@@ -42,21 +42,27 @@ function displayChart() {
       .on('mouseout', handleMouseout);
 
     function handleMouseover(d) {
-      const quarter = ['01', '04', '07', '10'];
+      const quarter = [
+        '01',
+        '04',
+        '07',
+        '10'
+      ];
       const tooltip = d3.select('.chart')
         .append('div')
         .attr('class', 'tooltip')
-        .style('opacity', 0)
+        .style('visibility', 'hidden');
 
       d3.select(this)
         .attr('fill', '#46d246');
 
       tooltip.transition()
         .duration(200)
-        .style('opacity', 0.9);
+        .style('visibility', 'visible');
+
       tooltip.html(`${d[0].split('-')[0]} Q${quarter.indexOf(d[0].split('-')[1]) + 1}<br/>$${d[1]}B`)
-        .style('left', `${d3.event.pageX - 130}px`)
-        .style('top', `${d3.event.pageY - 200}px`);
+        .style('left', `${d3.select(this).attr('x') - 100}px`)
+        .style('top', `${d3.event.pageY - 150}px`);
     }
 
     function handleMouseout() {
@@ -67,11 +73,20 @@ function displayChart() {
     }
 
     function resize() {
-      w = parseInt(d3.select('.chart').style('width')) * 0.9;
-      h = parseInt(d3.select('.chart').style('height'));
+      w = window.innerWidth * 0.9;
 
-      if (w < 600) {
-        w = 600;
+      if (w < 800) {
+        w = 800;
+        h = w * 0.8;
+      }
+      else {
+
+        if (window.innerWidth < window.innerHeight) {
+          h = window.innerHeight * 0.6;
+        }
+        else {
+          h = window.innerHeight * 0.8;
+        }
       }
 
       xScale.range([padding.left, w - padding.right]);
