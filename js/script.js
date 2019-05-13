a2dc5e3386e1227feb17ea39f83a5d2769a3a4e6
newcomet-lab <newcometcto729@gmail.com>
2019-05-13 15:04:33 -0400

    Improve responsiveness

--- js/script.js
+++ js/script.js
@@ -5,28 +5,35 @@ function displayChart() {
     bottom: 40,
     left: 110
   };
-  let w;
-  let h;
+  const w = 800;
+  const h = w * 0.5;
 
   d3.json('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json').then(dataset => {
     const xScale = d3.scaleTime()
-      .domain(d3.extent(dataset.data, (d) => new Date(d[0])));
+      .domain(d3.extent(dataset.data, (d) => new Date(d[0])))
+      .range([margin.left, w - margin.right]);
     const yScale = d3.scaleLinear()
-      .domain([0, d3.max(dataset.data, (d) => d[1])]);
+      .domain([0, d3.max(dataset.data, (d) => d[1])])
+      .range([h - margin.top, margin.bottom]);
+
     const svg = d3.select('.chart')
-      .append('svg');
+      .append('svg')
+      .attr('viewBox', `0 0 ${w} ${h}`);
 
     svg.append('g')
-      .attr('class', 'x-axis');
+      .attr('class', 'x-axis')
+      .attr('transform', `translate(0, ${h - margin.top})`)
+      .call(d3.axisBottom(xScale));
 
     svg.append('g')
       .attr('class', 'y-axis')
-      .attr('transform', `translate(${margin.left}, 0)`);
+      .attr('transform', `translate(${margin.left}, 0)`)
+      .call(d3.axisLeft(yScale));
 
     svg.append('text')
       .attr('class', 'y-label')
       .attr('transform', 'rotate(-90)')
-      .attr('x', -290)
+      .attr('x', -260)
       .attr('y', 40)
       .attr('fill', '#fff')
       .text('Gross Domestic Product in Billions');
@@ -36,6 +43,10 @@ function displayChart() {
       .enter()
       .append('rect')
       .attr('class', 'bar')
+      .attr('x', (d) => xScale(new Date(d[0])))
+      .attr('y', (d) => yScale(d[1]))
+      .attr('width', w / dataset.data.length)
+      .attr('height', (d) => h - yScale(d[1]) - margin.top)
       .attr('fill', '#4ddbff')
       .on('mouseover', handleMouseover)
       .on('mouseout', handleMouseout);
@@ -70,48 +81,7 @@ function displayChart() {
 
       d3.select('.tooltip').remove();
     }
-
-    function resize() {
-      w = window.innerWidth * 0.9;
-
-      if (w < 800) {
-        w = 800;
-        h = w * 0.8;
-      }
-      else {
-
-        if (window.innerWidth < window.innerHeight) {
-          h = window.innerHeight * 0.6;
-        }
-        else {
-          h = window.innerHeight * 0.8;
-        }
-      }
-
-      xScale.range([margin.left, w - margin.right]);
-      yScale.range([h - margin.top, margin.bottom]);
-
-      svg.attr('viewBox', `0 0 ${w} ${h}`);
-
-      svg.select('.x-axis')
-        .attr('transform', `translate(0, ${h - margin.top})`)
-        .call(d3.axisBottom(xScale));
-
-      svg.select('.y-axis')
-        .call(d3.axisLeft(yScale));
-
-      svg.selectAll('.bar')
-        .attr('x', (d) => xScale(new Date(d[0])))
-        .attr('y', (d) => yScale(d[1]))
-        .attr('width', w / dataset.data.length)
-        .attr('height', (d) => h - yScale(d[1]) - margin.top);
-    }
-
-    resize();
-
-    d3.select(window)
-      .on('resize', resize);
-  }).catch((err) => {
+  }).catch(err => {
     document.querySelector('.error-message').style.display = 'block';
   });
 }
